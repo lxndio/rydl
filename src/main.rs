835e9a3a004b2a1f28e924d2d4c983bf2cf94560
Matheus de Alcantara <matheus.de.alcantara@gmail.com>
2015-11-22 15:11:56 -0200

    initial thingy where you can type.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate ncurses;
+extern crate hlua;
+
+use ncurses::*;
+
 fn main() {
-    println!("Hello, world!");
+	initscr ();
+	refresh ();
+
+	let mut ch = getch();
+	let mut chars: Vec<char> = Vec::new();
+
+	while ch != 33 {
+		let chr: char = (ch as u8) as char;
+
+		match chr {
+			'\x1b' => {
+				endwin(); return;
+			},
+			_ => {
+				chars.push(chr)
+			}
+		}
+		ch = getch();
+	}
+
+	getch ();
+	endwin ();
 }
