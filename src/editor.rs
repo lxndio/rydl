40f88ed1a9a7c72af42201e292b8fa556bfa3c7b
Alexander Korn <alexander@korn-online.org>
2019-09-24 20:02:47 +0200

    Implemented basic cursor movement functions

--- src/editor.rs
+++ src/editor.rs
@@ -1,8 +1,8 @@
-use termion::{clear, color, terminal_size};
+use std::io::{stdin, stdout, Stdin, Stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin, Stdin, Stdout};
+use termion::{clear, color, terminal_size};
 
 #[derive(PartialEq)]
 pub enum EditorMode {
@@ -14,7 +14,7 @@ impl EditorMode {
     fn name(&self) -> String {
         match *self {
             EditorMode::Command => String::from("COMMAND"),
-            EditorMode::Insert  => String::from("INSERT"),
+            EditorMode::Insert => String::from("INSERT"),
         }
     }
 }
@@ -61,14 +61,16 @@ impl Editor {
     pub fn init(&mut self) {
         self.x = self.start_x;
         self.buffer.push(String::from("----- LINE 0 -----")); // Because lines are indexed from 1
+        self.buffer.push(String::new()); // Line 1 because a file always has at least one line
 
-        write!(stdout(),
-           "{}{}{}",
-           termion::clear::All,
-           termion::cursor::Goto(1, 1),
-           termion::cursor::Show)
-            .unwrap();
-        
+        write!(
+            stdout(),
+            "{}{}{}",
+            termion::clear::All,
+            termion::cursor::Goto(1, 1),
+            termion::cursor::Show
+        )
+        .unwrap();
         self.draw();
     }
 
@@ -84,45 +86,65 @@ impl Editor {
     fn draw_bar(&mut self) {
         let mut stdout = stdout().into_raw_mode().unwrap();
 
-        for x in 1..=self.width {
-            write!(stdout,
-                "{}{} ",
-                color::Bg(color::White),
-                termion::cursor::Goto(x, self.height-1)).unwrap();
-        }
+        let output = " ".repeat(self.width as usize);
+
+        write!(
+            stdout,
+            "{}{}{}",
+            color::Bg(color::White),
+            termion::cursor::Goto(1, self.height - 1),
+            output
+        )
+        .unwrap();
 
         if self.mode != EditorMode::Command {
-            write!(stdout,
+            write!(
+                stdout,
                 "{}{}-- {} --",
                 color::Fg(color::Black),
-                termion::cursor::Goto(2, self.height-1),
-                self.mode.name()).unwrap();
+                termion::cursor::Goto(2, self.height - 1),
+                self.mode.name()
+            )
+            .unwrap();
         }
 
-        write!(stdout,
+        write!(
+            stdout,
             "{}{}{}",
             color::Fg(color::Reset),
             color::Bg(color::Reset),
-            termion::cursor::Goto(self.x, self.y)).unwrap();
+            termion::cursor::Goto(self.x, self.y)
+        )
+        .unwrap();
     }
 
     fn draw_line_numbers(&mut self) {
         let mut stdout = stdout().into_raw_mode().unwrap();
+        let mut y = 1;
 
-        let to = if self.buffer.len()-self.top_line >= usize::from(self.height)-2 { usize::from(self.height)-2-self.top_line } else { self.buffer.len()-1 };
+        let to = if self.buffer.len() - self.top_line >= usize::from(self.height) - 2 {
+            usize::from(self.height) - 2 - self.top_line
+        } else {
+            self.buffer.len() - 1
+        };
 
-    	for y in self.top_line..=to {
-            let x = match y.to_string().len() {
+        for number in self.top_line..=to {
+            let x = match number.to_string().len() {
                 1 => 3,
                 2 => 2,
                 3 => 1,
                 _ => 1,
             };
 
-            write!(stdout,
+            write!(
+                stdout,
                 "{}{}",
                 termion::cursor::Goto(x, y as u16), // TODO probably replace as with try_from()
-                y).unwrap();
+                number
+            )
+            .unwrap();
+
+            y +=1 ;
         }
     }
 
@@ -130,41 +152,74 @@ impl Editor {
         let mut stdout = stdout().into_raw_mode().unwrap();
         let mut y = 1;
 
-        for i in self.top_line..=self.top_line + usize::from(self.height)-2 {
+        for i in self.top_line..=self.top_line + usize::from(self.height) - 2 {
             match self.buffer.get(i) {
                 Some(line) => {
-                    write!(stdout,
+                    write!(
+                        stdout,
                         "{}{}{}",
                         termion::cursor::Goto(self.start_x, y),
                         termion::clear::CurrentLine,
-                        line).unwrap();
-                },
-                None => {},
+                        line
+                    )
+                    .unwrap();
+                }
+                None => {}
             }
 
             y += 1;
         }
     }
-    
     fn draw_cursor(&mut self) {
         let mut stdout = stdout().into_raw_mode().unwrap();
 
-        write!(stdout,
-            "{}",
-            termion::cursor::Goto(self.x, self.y)).unwrap();
-    }
-
-    fn move_cursor_right(&mut self) {
-        self.x += 1;
+        write!(stdout, "{}", termion::cursor::Goto(self.x, self.y)).unwrap();
     }
 
     fn move_cursor_left(&mut self) {
-        self.x -= 1;
+        // TODO make + 4 variable depending on the length of the line numbers
+        if self.x > 1 + 4 {
+            self.x -= 1;
+        }
+    }
+
+    fn move_cursor_right(&mut self) {
+        // TODO make + 4 variable depending on the length of the line numbers
+        if (self.x as usize) <= self.buffer.get(self.current_line).unwrap().len() + 4 {
+            self.x += 1;
+        }
+    }
+
+    fn move_cursor_up(&mut self) {
+        if self.y > 1 {
+            self.y -= 1;
+            self.current_line -= 1;
+        }
+    }
+
+    fn move_cursor_down(&mut self) {
+        // TODO current_line and scrolling handling
+        if self.y < self.height - 2 {
+            self.y += 1;
+            self.current_line += 1;
+        }
     }
 
     fn move_cursor_new_line(&mut self) {
-        self.x = self.start_x;
-        self.y += 1;
+        // TODO current_line and scrolling handling
+        if self.y >= self.height - 3 {
+            self.x = self.start_x;
+            self.top_line += 1;
+        } else {
+            self.x = self.start_x;
+            self.y += 1;
+        }
+    }
+
+    fn move_cursor_eocl(&mut self) {
+        // TODO make + 4 variable depending on the length of the line numbers
+        self.x = (self.buffer.get(self.current_line).unwrap().len() + 1 + 4) as u16;
+        self.y = (self.current_line - self.top_line + 1) as u16;
     }
 
     fn handle_keys(&mut self) {
@@ -178,10 +233,22 @@ impl Editor {
                         self.mode = EditorMode::Insert;
                         self.draw();
                     },
+                    Key::Char('h') => {
+                        self.move_cursor_left();
+                    },
+                    Key::Char('j') => {
+                        self.move_cursor_down();
+                    },
+                    Key::Char('k') => {
+                        self.move_cursor_up();
+                    },
+                    Key::Char('l') => {
+                        self.move_cursor_right();
+                    },
                     Key::Esc => {
                         self.running = false;
                         break;
-                    }
+                    },
                     _ => {},
                 }
             } else if self.mode == EditorMode::Insert {
@@ -189,14 +256,9 @@ impl Editor {
                     Key::Char(c) => {
                         if c == '\n' {
                             self.current_line += 1;
-                            
                             self.move_cursor_new_line();
+                            self.buffer.push(String::new());
                         } else {
-                            // Add new line if it doesn't exist yet
-                            if self.current_line == self.buffer.len() {
-                                self.buffer.push(String::new());
-                            }
-
                             // unwrap should be save because current_line should always be in range per invariant
                             self.buffer.get_mut(self.current_line).unwrap().push(c);
 
@@ -205,12 +267,15 @@ impl Editor {
                     },
                     Key::Backspace => {
                         if self.buffer.get_mut(self.current_line).unwrap().pop() == None {
-                            self.buffer.remove(self.current_line);
-                            self.current_line -= 1; // TODO what if it was the first line?
+                            if self.current_line > 1 {
+                                self.buffer.remove(self.current_line);
+                                self.current_line -= 1;
+                                self.move_cursor_eocl();
+                            }
+                        } else {
+                            self.move_cursor_left();
                         }
-
-                        self.move_cursor_left();
-                    }
+                    },
                     Key::Esc => {
                         self.mode = EditorMode::Command;
                         self.draw();
